25451762c07f68364b2aea1a5fd47941ac587593
Quijano <angel.q.nolasco@gmail.com>
2018-09-20 18:22:26 -0500

    Vistas en el master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,23 @@ import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home.component';
+import { DondeLlegasComponent } from './donde-llegas/donde-llegas.component';
+import { ListComponent } from './list/list.component';
 import { NavbarComponent } from './components/navbar.component';
 import { FooterComponent } from './components/footer.component';
+import { AsientosComponent } from './asientos/asientos.component';
+import { PagoComponent } from './pago/pago.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    DondeLlegasComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ListComponent,
+    AsientosComponent,
+    PagoComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,22 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from  '@angular/router';
+import { ActivatedRoute } from "@angular/router";
 
 //Aqui importaremos las rutas de los componentes
 import { HomeComponent } from './components/home.component';
+import { DondeLlegasComponent } from './donde-llegas/donde-llegas.component';
+import { ListComponent } from './list/list.component';
+import { AsientosComponent } from './asientos/asientos.component';
+import { PagoComponent } from './pago/pago.component';
 
 //Estas son las rutas de nuestro proyecto que agregaremos poco a poco
 const appRoutes: Routes = [
 	{path: 'home', component: HomeComponent },
-	{path: '**', pathMatch: 'full', redirectTo: 'home' }
+	{path: 'donde-sales', component: DondeLlegasComponent},
+	{path: 'list', component: ListComponent},
+	{path: 'asientos', component: AsientosComponent},
+	{path: 'pago', component: PagoComponent},
+	{path: '**', pathMatch: 'full', redirectTo: 'donde-sales' }
 
 ];
 
