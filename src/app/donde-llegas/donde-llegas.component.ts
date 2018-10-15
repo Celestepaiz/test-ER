7968bd1d9c6f360cac30981894895688087ed89e
Suriel Asael <surielasaelrosasmendez@MacBook-Pro-de-Suriel.local>
2018-10-15 17:52:19 -0500

    Service for WebServer

--- src/app/donde-llegas/donde-llegas.component.ts
+++ src/app/donde-llegas/donde-llegas.component.ts
@@ -20,23 +20,61 @@ export class DondeLlegasComponent implements OnInit {
 	constructor(private _totemService: TotemApisService) { }
 
   	ngOnInit() {
-      this.iniciarSesion();
+      this.soapCall();
   	}
 
+    soapCall() {
+      const xmlhttp = new XMLHttpRequest();
+      xmlhttp.open('POST', 'http://187.210.68.147:8082/ER_WS_CONTROL/ERWSINFRAService?wsdl', true);
+      //const input_element = <HTMLInputElement> document.getElementById('choosenNumber');
+
+      //console.log('chVal : ' + input_element.value);
+      //const choosenNumberValue = input_element.value;
+
+      // The following variable contains the xml SOAP request.
+      const sr =
+          `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:con="http://control/">
+            <soapenv:Header/>
+            <soapenv:Body>
+              <con:getLogin>
+                <parameter>
+                  <canalVenta>TMS</canalVenta>
+                  <direccionIP>189.203.101.114</direccionIP>
+                  <direccionMAC>A0:21:B7:A9:05:C0</direccionMAC>
+                  <nombreCaja>CAJAWEB-ER</nombreCaja>
+                  <nombreEquipo>CAJAWEB-ER</nombreEquipo>
+                  <sucursalClave>WEB</sucursalClave>
+                  <usuarioContrasena>123456</usuarioContrasena>
+                  <usuarioNumero>1010</usuarioNumero>
+                </parameter>
+              </con:getLogin>
+            </soapenv:Body>`;
+
+      xmlhttp.onreadystatechange =  () => {
+          if (xmlhttp.readyState == 4) {
+              if (xmlhttp.status == 200) {
+                  const xml = xmlhttp.responseXML;
+                  // Here I'm getting the value contained by the <return> node.
+                  //const response_number = parseInt(xml.getElementsByTagName('return')[0].childNodes[0].nodeValue);
+                  // Print result square number.
+                  //console.log(response_number);
+              }
+          }
+      }
+      // Send the POST request.
+      xmlhttp.setRequestHeader('Content-Type', 'text/xml; charset=utf-8');
+      xmlhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+      xmlhttp.responseType = 'document';
+      xmlhttp.send(sr);
+    }
+
     iniciarSesion(){
-      this._totemService.getLogin().subscribe(
-        response => {
-            console.log(response);
+      this._totemService.getLoginPrueba().subscribe(
+        (result: any) => {
+
         },
         error => {
-          /*var errorMessage = <any>error;
-
-              if(errorMessage != null){
-                var body = JSON.parse(error._body);
-                this.alertMessage = body.message;
-                this.toastr.error(this.alertMessage, 'Error!');
-              }*/
-              console.log('Ocurrio un error');
+          console.log("Ocurrio un erro")
         }
       );
     }
--- src/app/services/totem-apis.service.ts
+++ src/app/services/totem-apis.service.ts
@@ -8,16 +8,19 @@ import { Observable } from 'rxjs';
 })
 export class TotemApisService {
 
-  constructor(private _http: Http) { }  
+  constructor(private _http: Http) { }
 
-  getLogin(){
+  getLoginPrueba(){
+    /*let headers = new Headers({
+      'Content-Type':'application/xml'
+    });*/
+
+    let url = 'http://187.210.68.147:8082/ER_WS_CONTROL/ERWSINFRAService?wsdl';
+
+    //let options = new RequestOptions({ headers: headers });
     let headers = new Headers({
-      'Content-Type':'application/xml',
-      'Accept':  'application/xml',
-      'Response-Type': 'text'
-    });
-
-    let options = new RequestOptions({ headers: headers });
+				'Content-Type':'application/xml'
+			});
 
     const postedData =
     `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:con="http://control/">
@@ -37,6 +40,50 @@ export class TotemApisService {
         </con:getLogin>
       </soapenv:Body>`;
 
-    return this._http.get(encodeURIComponent(postedData), options).pipe(map(res => res.text()));
+    return this._http.post(url, postedData, {headers: headers}).pipe(map(res => res.text()));
+  }
+
+  getLogin(){
+    return new Promise(function(resolve, reject){
+			var xhr = new XMLHttpRequest();
+
+      //let url = 'http://187.210.68.147:8082/ERWSINFRAService?wsdl';
+      let url = 'http://187.210.68.147:8082/ER_WS_CONTROL/ERWSINFRAService?wsdl'
+
+      const postedData =
+      `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:con="http://control/">
+        <soapenv:Header/>
+        <soapenv:Body>
+          <con:getLogin>
+            <parameter>
+              <canalVenta>TMS</canalVenta>
+              <direccionIP>189.203.101.114</direccionIP>
+              <direccionMAC>A0:21:B7:A9:05:C0</direccionMAC>
+              <nombreCaja>CAJAWEB-ER</nombreCaja>
+              <nombreEquipo>CAJAWEB-ER</nombreEquipo>
+              <sucursalClave>WEB</sucursalClave>
+              <usuarioContrasena>123456</usuarioContrasena>
+              <usuarioNumero>1010</usuarioNumero>
+            </parameter>
+          </con:getLogin>
+        </soapenv:Body>`;
+
+      xhr.onreadystatechange =  () => {
+          if (xhr.readyState == 4) {
+              if (xhr.status == 200) {
+                  const xml = xhr.responseXML;
+                  // Here I'm getting the value contained by the <return> node.
+                  //const response_number = parseInt(xml.getElementsByTagName('return')[0].childNodes[0].nodeValue);
+                  // Print result square number.
+                  //console.log(response_number);
+              }
+          }
+      }
+      xhr.open('POST', url, true);
+
+			//xhr.open('POST', url, true);
+			//xhr.setRequestHeader('Authorization', token);
+			xhr.send(postedData);
+		});
   }
 }
