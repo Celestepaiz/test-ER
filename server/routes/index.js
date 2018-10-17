6aac0450a9873f17cd3f45c8a1e597de763b33a5
Celestepaiz <paiz_azul@hotmail.com>
2018-10-17 16:24:11 -0500

    getOrigenesDestinosER, getBloquearAsientos, getVenderAsientos controllers

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,5 +7,8 @@ var api = express.Router();
 
 api.get('/probando-controlador', MainController.pruebas);
 api.get('/get-asientos-disp', MainController.getAsientosDisp)
+api.get('/get-origenes-destinos', MainController.getOrigenesDestinosEr)
+api.get('/get-bloquear-asientos', MainController.getBloquearAsientos)
+api.get('/get-vender-boletos', MainController.getVenderBoletos)
 
 module.exports = api;
