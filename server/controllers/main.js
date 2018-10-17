a721483009d14741b1ee062c185073473ff5af4c
Celestepaiz <paiz_azul@hotmail.com>
2018-10-17 15:13:44 -0500

    getAsientosDisp controller

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -1,21 +1,18 @@
 'use strict'
-
-//const EasySoap = require('easysoap');
 const soapRequest = require('easy-soap-request');
 const fs = require('fs');
 const convert = require('xml-js');
 //var url = 'http://187.210.68.147:8082/ER_WS_CONTROL/ERWSINFRAService?wsdl';
 var url = 'http://187.210.68.147:8082/ER_WS_CONTROL/ERWSINFRAService';
-
+var loginStatus = false;
 
 function pruebas(req, res){
-  
-
 	const headers = {
   'user-agent': 'sampleTest',
   'Content-Type': 'text/xml;charset=UTF-8',
   'soapAction': 'http://187.210.68.147:8082/ER_WS_CONTROL/ERWSINFRAService?wsdl#getLogin',
 };
+
 
 const xml = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:con="http://control/">
    <soapenv:Header/>
@@ -34,26 +31,64 @@ const xml = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/en
       </con:getLogin>
    </soapenv:Body>`;
 
-
 soapRequest(url, headers, xml)
  .then((response) => {
+  loginStatus = true;
    console.log(response.response.body);
    const xmlres = response.response.body
-   const result = convert.xml2json(xmlres, {compact: false, alwaysArray: true});
-
-   console.log(result)
-   
+   const result = convert.xml2json(xmlres, {compact: false, alwaysArray: false});
 	 res.status(200).send({
-		 response
+     response
 	 });
  })
  .catch((err) => {
    console.log(err)
-
  });
 
-}
+ 
 
+}
+function getAsientosDisp(req, res){
+  var params = req.body;
+  var claveCorrida = '4PTEP0610N838900';
+  var origen = '4PTE';
+  var sesionId = '3730';
+
+  const headers = {
+    'user-agent': 'sampleTest',
+    'Content-Type': 'text/xml;charset=UTF-8',
+    'soapAction': 'http://187.210.68.147:8082/ER_WS_CONTROL/ERWSINFRAService?wsdl#getAsientosDisp',
+  };
+  const xml = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:con="http://control/">
+   <soapenv:Header/>
+   <soapenv:Body>
+   <con:getAsientosDisp>
+   <!--Optional:-->
+   <request>
+   <!--Optional:-->
+   <claveCorrida>${claveCorrida}</claveCorrida>
+   <!--Optional:-->
+   <origen>${origen}</origen>
+   <sesionId>${sesionId}</sesionId>
+   </request>
+   </con:getAsientosDisp>
+   </soapenv:Body>
+  </soapenv:Envelope>`;
+  soapRequest(url, headers, xml)
+  .then((response) => {
+    console.log(response.response.body);
+    /*
+    const xmlres = response.response.body
+    const result = convert.xml2json(xmlres, {compact: false, alwaysArray: true});
+    console.log(response)*/
+    res.status(200).send({response});
+  })
+  .catch((err) => {
+    console.log(err)
+  
+  });
+}
 module.exports = {
-	pruebas
+  pruebas,
+  getAsientosDisp
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,5 +6,6 @@ var MainController = require('../controllers/main');
 var api = express.Router();
 
 api.get('/probando-controlador', MainController.pruebas);
+api.get('/get-asientos-disp', MainController.getAsientosDisp)
 
 module.exports = api;
