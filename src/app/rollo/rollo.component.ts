d055663891b6bc833f4f0c636687a3456c2af912
Christian Romero <christianrh14@outlook.com>
2018-11-13 21:43:30 -0600

    Modificaciones diseno Tourister Chris

--- src/app/rollo/rollo.component.ts
+++ src/app/rollo/rollo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbPanelChangeEvent} from '@ng-bootstrap/ng-bootstrap';
 
+declare let paypal: any;
 
 @Component({
   selector: 'app-rollo',
@@ -22,6 +23,52 @@ export class RolloComponent implements OnInit {
     }
   }
 
+  addScript: boolean = false;
+  paypalLoad: boolean = false;
+
+  finalAmount: number = 1;
+
+  paypalconfig = {
+    env: 'sandbox',
+    client: {
+      sandbox: 'AWXKT42pvW0yB-LHRMphM2t-rn4FS9pIldA7DYZdCaziB_bMeGXNsDeQYtXjleYxtM0K38aCTWwEwLGs' 
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: this.finalAmount, currency: 'USD'}}
+          ]
+        }
+      })
+    },
+
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
+        //do somenthing when payment is successful
+      })
+    }
+  };
+
+  ngAfterViewChecked(): void{
+    if(!this.addScript){
+      this.addPaypalScript().then(() => {
+          paypal.Button.render(this.paypalconfig, '#paypal-button-container');
+          this.paypalLoad = false;
+      })
+    }
+  }
+
+  addPaypalScript(){
+    this.addScript = true;
+    return new Promise((resolve, reject) => {
+      let scripttagElement = document.createElement('script');
+      scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scripttagElement.onload = resolve;
+      document.body.appendChild(scripttagElement);
+    })
+  } 
   
 
   constructor() { }
